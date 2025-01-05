c40c04f97ff89784bdb6afe4bb9e24aa13aa9e20
悔悟 <95004796+yizhaol@users.noreply.github.com>
2025-01-05 23:00:20 +0800

    Update Function.php

--- admin/Function.php
+++ admin/Function.php
@@ -1,22 +1,4 @@
 <?php
-
-/*
- * @Page：自定义函数方法
- * @Version：Like Girl 5.2.0
- * @Author: Ki.
- * @Date: 2024-11-08 10:00:00
- * @LastEditTime: 2024-11-08
- * @Description: 愿得一人心 白首不相离
- * @Document：https://blog.kikiw.cn/index.php/archives/52/
- * @Copyright (c) 2024 by Ki All Rights Reserved. 
- * @Warning：禁止以任何方式出售本项目 如有发现一切后果自行负责
- * @Warning：禁止以任何方式出售本项目 如有发现一切后果自行负责
- * @Warning：禁止以任何方式出售本项目 如有发现一切后果自行负责
- * @Message：开发不易 版权信息请保留 （删除/更改版权的无耻之人请勿使用 查到一个挂一个）
- * @Message：开发不易 版权信息请保留 （删除/更改版权的无耻之人请勿使用 查到一个挂一个）
- * @Message：开发不易 版权信息请保留 （删除/更改版权的无耻之人请勿使用 查到一个挂一个）
- */
- 
 if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
     $list = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
     $_SERVER['REMOTE_ADDR'] = $list[0];
